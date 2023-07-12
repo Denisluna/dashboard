c24af7df6cbf579ef9b57f8623970dbdd5b237b9
= <=>
2023-07-11 21:35:47 -0300

    create sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,50 @@
-import React from 'react'
+import { Link, NavLink } from "react-router-dom"
+import { SiShopware } from 'react-icons/si'
+import { MdOutlineCancel } from 'react-icons/md'
+import { TooltipComponent } from "@syncfusion/ej2-react-popups"
+
+import { links } from '../data/dummy'
 
 const Sidebar = () => {
+  const activeMenu = true;
+
+  const activeLink = 'text-white bg-gray-700'
+  const normal = 'text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray'
+
   return (
-    <div>Sidebar</div>
+    <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
+      {activeMenu && (<>
+        <div className="flex justify-between items-center mt-4 ml-3 mr-3">
+          <Link to='/' onClick={() => { }} className="flex items-center gap-3 text-xl font-extrabold tracking-tight dark:text-white text-slate-900">
+            <SiShopware /><span>Shoppy</span>
+          </Link>
+          <TooltipComponent content="Menu" position="BottomCenter">
+            <button type="button"
+              className="text-xl hover:bg-light-gray rounded-full p-3"
+              onClick={() => { }}>
+              <MdOutlineCancel />
+            </button>
+          </TooltipComponent>
+        </div>
+        <div className="mt-10">
+          {links.map((item) => (
+            <div key={item.title}>
+              <h2 className="text-gray-400 m-3 mt-4 uppercase">{item.title}</h2>
+              {item.links.map((link) => (
+                <NavLink
+                  to={`/${link.name}`}
+                  key={link.name}
+                  onClick={() => { }}
+                  className={({ isActive }) => `flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md m-2 ${isActive ? activeLink : normal}`}>
+                    {link.icon}
+                    <span className="capitalize">{link.name}</span>
+                </NavLink>
+              ))}
+            </div>
+          ))}
+        </div>
+      </>)}
+    </div>
   )
 }
 
